d38ef1a1f4e4ac82f486fd3617a9344453baaa51
vlad <vlad.barosan@gmail.com>
2012-10-27 20:24:39 +0300

    Added base class for handlers

--- yocto.py
+++ yocto.py
@@ -1,25 +1,34 @@
 import webapp2
 import jinja2
-
+import models.dbModels
 
 jinja_env = jinja2.Environment( loader =
     jinja2.FileSystemLoader('./templates/'), autoescape = True )
 
 
-class MainPageHandler( webapp2.RequestHandler ):
+
+class BaseHandler( webapp2.RequestHandler ):
+    """Base class for handlers"""
+    
+    def render( self, template, **kwargs):
+    """ Renders the template 'template' with the values from kwargs"""
+
+        page_template = jinja_env.get_template( template )
+        rendered_page = page_template( **kwargs )
+        self.response.out( rendered_page )
+
+    
+
+
+class MainPageHandler( BaseHandler ):
 
     def get(self):
-
-        self.template = jinja_env.get_template( 'extended.html' )
-        self.rendered_template = self.template.render()
-        self.response.out.write( self.rendered_template )
+        self.render ( 'mainpage.html')
 
 
 
 
 
 
-
-
-app = webapp2.WSGIApplication( [ ('/.*', MainPageHandler )
+app = webapp2.WSGIApplication( [ ('/', MainPageHandler )
                                 ], debug=True )
