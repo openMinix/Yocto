94fc37f8c16cd67d7346a20711beebf496d95e1b
Nicu <nicu@nicu-Satellite-L300.(none)>
2012-10-27 20:11:45 +0300

    added post model

---
+++ models/dbModels.py
@@ -0,0 +1,14 @@
+from google.appengine.ext import db
+import datetime
+
+
+class Post(db.Model):
+    content = db.TextProperty()
+    name    = db.StringProperty()
+    date    = db.DateTimeProperty()
+    votes   = db.IntegerProperty()
+
+    @classmethod
+    def get_by_name(cls, name):
+        return cls.all().filter("name =", name).get()
+
